a3d4abf77e78cfcd9449f5d8d0aab1ba6c235bed
mac <mac@macs-MacBook-Air.local>
2024-06-02 16:35:00 -0400

    created three environments for development

---
+++ src/main/java/com/capstone/closetconnect/ClosetconnectApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.closetconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosetconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosetconnectApplication.class, args);
+	}
+
+}
