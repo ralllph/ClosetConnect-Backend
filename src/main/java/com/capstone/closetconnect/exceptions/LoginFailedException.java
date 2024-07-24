39921c111289532917c3b5e6e1f5ff4c23ded8d1
mac <Oizueke8548@conestogac.on.ca>
2024-07-24 12:03:28 -0400

    catching failed login or token exception

--- src/main/java/com/capstone/closetconnect/exceptions/LoginFailedException.java
+++ src/main/java/com/capstone/closetconnect/exceptions/LoginFailedException.java
@@ -2,6 +2,6 @@ package com.capstone.closetconnect.exceptions;
 
 public class LoginFailedException extends RuntimeException{
     public LoginFailedException(){
-        super("Invalid username or Password. Please try again");
+        super("Invalid credentials or token. consider checking credentials and expiry");
     }
 }
